2b0adb5c309ba04de6707883a6e378c0f55a34c3
sivadmaddy-cloud <sivadmaddy@gmail.com>
2026-03-09 14:10:22 +0530

    modify the code

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://module-management.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
@@ -15,3 +15,4 @@ API.interceptors.request.use((req) => {
 });
 
 export default API;
+
